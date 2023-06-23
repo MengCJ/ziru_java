1a35700d3c399b40e607ce17bba8ba6672b0263f
waykom <waykom1@gmail.com>
2023-06-23 23:54:44 +0800

    userAPI

--- src/main/java/com/qfedu/springboot/config/Contants.java
+++ src/main/java/com/qfedu/springboot/config/Contants.java
@@ -2,6 +2,7 @@ package com.qfedu.springboot.config;
 
 public interface Contants {
     int DELETE_FLAG = 0;
+    int TEST = 1;
     int UN_DELETE_FLAG = 1;
     Boolean ENABLED = true;
     Boolean UN_ENABLED = false;
